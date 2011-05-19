1dacfdca572a5e4f0874706cf8891152f7975d55
Mike Jones <mike.a.jones@bristol.ac.uk>
2011-05-19 11:55:27 +0100

    tidied up the plugin

--- MobileWeb/mca-maven-plugins/data-maven-plugin/src/main/java/org/ilrt/mca/plugins/FindDataFilesMojo.java
+++ MobileWeb/mca-maven-plugins/data-maven-plugin/src/main/java/org/ilrt/mca/plugins/FindDataFilesMojo.java
@@ -1,21 +1,36 @@
 package org.ilrt.mca.plugins;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Copyright (c) 2009, University of Bristol
+ * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 1) Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 2) Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3) Neither the name of the University of Bristol nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
  */
-
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
@@ -26,25 +41,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * @goal hiya
+ * Finds all the registry and local data files to create a manifest of their
+ * names and locations. The system can the find and load the files via a
+ * manifest into the database.
+ *
+ * @goal find-data
  */
 public class FindDataFilesMojo extends AbstractMojo {
 
     /**
-     * The directory for the generated WAR.
+     * The directory for the generated classes
      *
-     * @parameter default-value="${project.build.directory}/classes/"
+     * @parameter default-value="${project.build.directory}/classes/".
      * @required
      */
-    private File outputDirectory;
+    private File outputDirectory = null;
 
     /**
-     * The base directory for the local data.
+     * The resources directory (base directory for the local data).
      *
-     * @parameter default-value="${basedir}/src/main/resources/"
+     * @parameter default-value="${basedir}/src/main/resources/".
      * @required
      */
-    private File resourcesDir;
+    private File resourcesDir = null;
 
     /**
      * The base directory for the registry data.
@@ -52,34 +71,46 @@ public class FindDataFilesMojo extends AbstractMojo {
      * @parameter default-value="${basedir}/src/main/resources/data/registry/"
      * @required
      */
-    private File registryDir;
+    private File registryDir = null;
 
     /**
-     * The base directory for the registry data.
+     * The base directory for other data to be placed in named graphs.
      *
      * @parameter default-value="${basedir}/src/main/resources/data/graph/"
      * @required
      */
-    private File graphDir;
+    private File graphDir = null;
+
+    /**
+     * The name of the data manifest file
+     * @parameter default-value="data-manifest.txt"
+     */
+    private String manifestFileName = null;
+
+    public FindDataFilesMojo() {}
 
     public void execute() throws MojoExecutionException {
 
+        // hold the location of the files
         List<String> registryFiles = new ArrayList<String>();
         List<String> graphFiles = new ArrayList<String>();
 
+        // process the registry files
         processDirectory(registryDir, registryFiles);
+
+        // process the local data files
         processDirectory(graphDir, graphFiles);
 
 
         File f = outputDirectory;
 
-        File output = new File(f, "data.txt");
+        File output = new File(f, manifestFileName);
 
         FileWriter writer = null;
         try {
             writer = new FileWriter(output);
 
-            writer.write("# This is an automatically generated file");
+            writer.write("# This is an automatically generated file\n");
 
             for (String val : registryFiles) {
                 writer.write("registry:" + val + "\n");
@@ -100,23 +131,25 @@ public class FindDataFilesMojo extends AbstractMojo {
                 }
             }
         }
-
-
     }
 
-    private void processDirectory(File f, List list) {
+    /**
+     * Search directories and sub directories for data files.
+     *
+     * @param f a file or a directory.
+     * @param list list of found data files.
+     */
+    private void processDirectory(File f, List<String> list) {
         if (f.exists()) {
-            if (f.isDirectory()) {
+            if (f.isDirectory()) {  // if its a directory, look for its contents
                 File[] files = f.listFiles();
-                for (int i = 0; i < files.length; i++) {
-                    processDirectory(files[i], list);
+                for (File file : files) {
+                    processDirectory(file, list); // recursive call to deal with contents
                 }
-            } else {
+            } else { // its a file so add its location to the list
                 list.add(f.getAbsolutePath().substring(resourcesDir.getAbsolutePath().length()));
             }
         }
     }
-
-
 
 }
